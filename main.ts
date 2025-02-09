6e13c945d9ef2237a3307a48daee6ec554a4b3de
Nikitoobg <nik.papazoff@gmail.com>
2025-02-09 18:16:18 +0200

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,23 @@
+import Parser from "./frontend/parser.ts";
+import {evaluate} from "./runtime/interpreter.ts";
+
+repl();
+
+function repl() {
+  const parser = new Parser();
+  console.log("Repl v0.1");
+
+  while (true) {
+    const input = prompt("> ");
+    // Check for no user input or exit keyword.
+    if (!input || input.includes("exit")) {
+      Deno.exit(1);
+    }
+
+    const program = parser.produceAST(input);
+
+    const result = evaluate(program);
+    console.log(result);
+
+  }
+}
